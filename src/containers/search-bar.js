f2302b7c752e5efacbc7f7d835f94a1f1da8e0b6
Jun <junoh421@gmail.com>
2017-12-20 13:01:20 -0500

    cleanup search bar

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -4,13 +4,41 @@ import { selectUser } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''} ;
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value})
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.selectUser(this.props.users[0]);
+    this.setState( {term: ''} );
+  }
+
+
   render() {
     return (
-      <div className="search-bar">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
-          onChange={event => this.props.selectUser(this.props.users[1]) }
+         placeholder="Search"
+         className="form-control"
+         value={this.state.term}
+         onChange={this.onInputChange}
         />
-      </div>
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
     ); 
   }
 }
