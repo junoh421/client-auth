3f1f574b8626a2988bad95f3b824293e0049a3fe
Jun <junoh421@gmail.com>
2017-12-23 15:33:39 -0500

    basic redux form with errors and set up router for different paths

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-
 import App from './components/app';
+import NewUser from './components/new_user';
+import LoginUser from './components/login_user';
 import reducers from './reducers';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './style/index.css';
-
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  	<BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/login' component={LoginUser} />
+          <Route path='/users/new' component={NewUser} />
+          <Route path='/' component={App} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import MatchingUser from './reducer_matching_user';
 import Users from './reducer_users';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
 	users: Users,
-    matchingUser: MatchingUser
+    matchingUser: MatchingUser,
+    form: formReducer
 });
 
 export default rootReducer;
